7ec407264093f29e3491593d8f48731d6d46b035
Jake Bowhay <jb9.bowhay@gmail.com>
2026-08-14 06:46:55 +0100

    benchmark cpu devices

--- scipy-2.0/scripts/benchmark_kurtosis.py
+++ scipy-2.0/scripts/benchmark_kurtosis.py
@@ -27,11 +27,28 @@ REPEATS = 10
 ns = np.logspace(2, 7, 6, dtype=int)
 times = []
 
+if "PyTorch" in backend:
+    import torch
+if "JAX" in backend:
+    import jax.numpy as jnp
+if backend == "JAX-CPU-JIT":
+    from jax import jit
+    kurtosis = jit(kurtosis)
+
 for n in ns:
     data = rng.gamma(5, 0.5, size=n)
 
     if backend == "NumPy":
+        kurtosis(data)
         times.append(timeit.timeit(lambda: kurtosis(data), number=REPEATS))
+    elif backend == "PyTorch-CPU":
+        data_torch = torch.asarray(data)
+        kurtosis(data_torch)
+        times.append(timeit.timeit(lambda: kurtosis(data_torch), number=REPEATS))
+    elif "JAX-CPU" in backend:
+        data_jax = jnp.asarray(data)
+        kurtosis(data_jax).block_until_ready()
+        times.append(timeit.timeit(lambda: kurtosis(data_jax).block_until_ready(), number=REPEATS))
 
 with open("scripts/benchmark_kurtosis_results.jsonl", "a") as f:
     f.write(json.dumps({"backend": backend, "ns": ns.tolist(), "times": times}) + "\n")
--- scipy-2.0/scripts/run_benchmark_kurtosis.sh
+++ scipy-2.0/scripts/run_benchmark_kurtosis.sh
@@ -3,13 +3,16 @@ set -euo pipefail
 
 rm -f scripts/benchmark_kurtosis_results.jsonl
 
-for backend in NumPy; do
+for backend in NumPy PyTorch-CPU JAX-CPU JAX-CPU-JIT; do
     echo
     echo "=============================="
     echo " Benchmarking: $backend"
     echo "=============================="
 
-    pixi run -e "${backend,,}" python scripts/benchmark_kurtosis.py $backend
+    env="${backend,,}"
+    env="${env%-jit}"
+
+    pixi run -e "$env" python scripts/benchmark_kurtosis.py $backend
 done
 
 pixi run -e numpy python scripts/benchmark_kurtosis.py plot
