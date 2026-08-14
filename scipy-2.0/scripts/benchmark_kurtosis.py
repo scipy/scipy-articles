9aa008ee9da52a0d2cd02abc3d64a7d0fcfcfcae
Jake Bowhay <jb9.bowhay@gmail.com>
2026-08-14 06:25:47 +0100

    start adding benchmark

--- scipy-2.0/scripts/benchmark_kurtosis.py
+++ scipy-2.0/scripts/benchmark_kurtosis.py
@@ -1,9 +1,26 @@
 import os
 os.environ["SCIPY_ARRAY_API"] = "1"
 import timeit
+import sys
+import json
 
 from scipy.stats import kurtosis
 import numpy as np
+backend = sys.argv[1]
+
+if backend == "plot":
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(layout="constrained")
+
+    with open("scripts/benchmark_kurtosis_results.jsonl") as f:
+        for line in f:
+            data = json.loads(line)
+            ax.loglog(data["ns"], data["times"], label=data["backend"])
+    ax.set_xlabel("n")
+    ax.set_ylabel("time (s)")
+    ax.legend()
+    plt.show()
 
 rng = np.random.default_rng(738274923759827)
 REPEATS = 10
@@ -13,5 +30,8 @@ times = []
 for n in ns:
     data = rng.gamma(5, 0.5, size=n)
 
-    if os.environ["BACKEND"] == "numpy":
+    if backend == "NumPy":
         times.append(timeit.timeit(lambda: kurtosis(data), number=REPEATS))
+
+with open("scripts/benchmark_kurtosis_results.jsonl", "a") as f:
+    f.write(json.dumps({"backend": backend, "ns": ns.tolist(), "times": times}) + "\n")
--- scipy-2.0/scripts/run_benchmark_kurtosis.sh
+++ scipy-2.0/scripts/run_benchmark_kurtosis.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for backend in numpy; do
+rm -f scripts/benchmark_kurtosis_results.jsonl
+
+for backend in NumPy; do
     echo
     echo "=============================="
     echo " Benchmarking: $backend"
     echo "=============================="
 
-    BACKEND="$backend" pixi run -e "$backend" python scripts/benchmark_kurtosis.py
+    pixi run -e "${backend,,}" python scripts/benchmark_kurtosis.py $backend
 done
+
+pixi run -e numpy python scripts/benchmark_kurtosis.py plot
