fb6ddd095ed55ddba697b81fa98b55dd5be4a48b
Jake Bowhay <jb9.bowhay@gmail.com>
2026-08-16 18:13:12 +0100

    coverage table improvements

--- scipy-2.0/scripts/array_api_compatibility_table.py
+++ scipy-2.0/scripts/array_api_compatibility_table.py
@@ -24,41 +24,81 @@ flat_table = make_flat_capabilities_table(included_modules, "cpu")
 
 df_cpu = pd.DataFrame.from_dict(calculate_table_statistics(flat_table), orient="index")
 df_cpu.drop(columns=["dask"], inplace=True)
-cols = ["torch", "jax"]
-df_cpu = df_cpu[cols].div(df_cpu["total"], axis=0).multiply(100).round().astype("Int64")
-df_cpu.rename(columns={col: f"{col}_cpu" for col in cols}, inplace=True)
+df_cpu.rename(
+    columns={col: f"{col}_cpu" for col in df_cpu.columns if col != "total"},
+    inplace=True,
+)
+df_cpu_percent = (
+    df_cpu.loc[:, df_cpu.columns != "total"]
+    .div(df_cpu["total"], axis=0)
+    .multiply(100)
+    .round()
+    .astype("Int64")
+)
 
 flat_table = make_flat_capabilities_table(included_modules, "gpu")
 
 df_gpu = pd.DataFrame.from_dict(calculate_table_statistics(flat_table), orient="index")
-cols = ["cupy", "torch", "jax"]
-df_gpu = df_gpu[cols].div(df_gpu["total"], axis=0).multiply(100).round().astype("Int64")
-df_gpu.rename(columns={col: f"{col}_gpu" for col in cols}, inplace=True)
+df_gpu.rename(
+    columns={col: f"{col}_gpu" for col in df_gpu.columns if col != "total"},
+    inplace=True,
+)
+df_gpu_percent = (
+    df_gpu.loc[:, df_gpu.columns != "total"]
+    .div(df_gpu["total"], axis=0)
+    .multiply(100)
+    .round()
+    .astype("Int64")
+)
 
+flat_table = make_flat_capabilities_table(included_modules, "jit")
 df_jit = pd.DataFrame.from_dict(calculate_table_statistics(flat_table), orient="index")
-cols = ["jax"]
-df_jit = df_jit[cols].div(df_jit["total"], axis=0).multiply(100).round().astype("Int64")
-df_jit.rename(columns={col: f"{col}jit" for col in cols}, inplace=True)
+df_jit.rename(
+    columns={col: f"{col}_jit" for col in df_jit.columns if col != "total"},
+    inplace=True,
+)
+df_jit_percent = (
+    df_jit.loc[:, df_jit.columns != "total"]
+    .div(df_jit["total"], axis=0)
+    .multiply(100)
+    .round()
+    .astype("Int64")
+)
 
-df = df_cpu.join(df_gpu, how="outer").join(df_jit, how="outer")
-# reorder the columns
-df = df[["cupy_gpu", "torch_cpu", "torch_gpu", "jax_cpu", "jax_gpu", "jaxjit"]]
+df_percent = df_cpu_percent.join(df_gpu_percent, how="outer").join(
+    df_jit_percent, how="outer"
+)
+df_percent = df_percent[
+    ["cupy_gpu", "torch_cpu", "torch_gpu", "jax_cpu", "jax_gpu", "jax_jit"]
+]
+df_percent.index = df_percent.index.str.replace("scipy.", "", regex=False)
+df_percent.index = [rf"\texttt{{{idx}}}" for idx in df_percent.index]
 
-df.index = df.index.str.replace("scipy.", "", regex=False)
-
-mean_row = df.mean(numeric_only=True).round().astype("Int64")
-mean_row.name = "mean"
+df_count = df_cpu.join(df_gpu.drop(columns="total"), how="outer").join(
+    df_jit.drop(columns="total"), how="outer"
+)
+df_count = df_count[
+    ["total", "cupy_gpu", "torch_cpu", "torch_gpu", "jax_cpu", "jax_gpu", "jax_jit"]
+]
+overall_count = df_count.sum(axis=0)
+overall_percent = (
+    overall_count.loc[overall_count.index != "total"]
+    .div(overall_count["total"])
+    .multiply(100)
+    .round()
+    .astype("Int64")
+)
 
 # add percent sign to all values
-df = df.astype(str) + r" \%"
-latex = df.to_latex()
+df_percent = df_percent.astype(str) + r" \%"
+latex = df_percent.to_latex()
 # add midrule before the mean row
 latex = latex.replace(
     r"\bottomrule",
     r"\midrule"
     + "\n"
-    + "mean & "
-    + " & ".join(mean_row.astype(str) + r" \%")
+    + "Overall & "
+    + " & ".join(overall_percent.astype(str) + r" \%")
     + r" \\"
     + "\n"
     + r"\bottomrule",
